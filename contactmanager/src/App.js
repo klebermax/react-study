c16573015a8e54e065c0e4ca19c636ebc89f3094
Kleber Maximiano <the.key.max@gmail.com>
2018-08-10 17:33:06 -0300

    Adding initial contact manager template project

---
+++ contactmanager/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Contact from './components/Contact';
+import './App.css';
+import Header from './components/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        <Contact
+          name="Karen Smith"
+          email="karen@gmail.com"
+          phone="333-333-5555"
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
