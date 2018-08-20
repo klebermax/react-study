1b0ffcf23f7f66afa5db0e0294282b4f666db743
Kleber Maximiano <the.key.max@gmail.com>
2018-08-20 10:07:41 -0300

    PUT Request & Update Action

--- contactmanager/src/components/contacts/Contact.js
+++ contactmanager/src/components/contacts/Contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Consumer } from '../../context';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 class Contact extends Component {
@@ -44,6 +45,18 @@ class Contact extends Component {
                   style={{ cursor: 'pointer', float: 'right', color: 'red' }}
                   onClick={this.onDeleteClick.bind(this, id, dispatch)}
                 />
+
+                <Link to={`contact/edit/${id}`}>
+                  <i
+                    className="fas fa-pencil-alt"
+                    style={{
+                      cursor: 'pointer',
+                      float: 'right',
+                      color: 'black',
+                      marginRight: '1rem'
+                    }}
+                  />
+                </Link>
               </h4>
               {showContactInfo ? (
                 <ul className="list-group">
