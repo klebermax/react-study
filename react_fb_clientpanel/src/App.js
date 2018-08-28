0e6de0a9cb00d646a5ce9bffb6d334d82c53a325
Kleber Maximiano <the.key.max@gmail.com>
2018-08-28 16:13:44 -0300

    Login & Logout

--- react_fb_clientpanel/src/App.js
+++ react_fb_clientpanel/src/App.js
@@ -11,6 +11,8 @@ import AddClient from './components/clients/AddClient';
 import ClientDetails from './components/clients/ClientDetails';
 import EditClient from './components/clients/EditClient';
 
+import Login from './components/auth/Login';
+
 import './App.css';
 
 class App extends Component {
@@ -26,6 +28,7 @@ class App extends Component {
                 <Route exact path="/client/add" component={AddClient} />
                 <Route exact path="/client/edit/:id" component={EditClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </div>
