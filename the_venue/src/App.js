295bc07b73ae9506dcca666086d7a3a727926652
Kleber Maximiano <the.key.max@gmail.com>
2018-08-31 16:18:09 -0300

    Header & carrousel

--- the_venue/src/App.js
+++ the_venue/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './resources/styles.css';
 
 import Header from './components/layout/Header';
+import Featured from './components/featured';
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height: '1500px', background: 'gray' }}>
         <Header />
-        hello oooo
+        <Featured />
       </div>
     );
   }
---
+++ the_venue/src/components/featured/Carrousel.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+import slide_one from '../../resources/images/slide_one.jpg';
+import slide_two from '../../resources/images/slide_two.jpg';
+import slide_three from '../../resources/images/slide_three.jpg';
+
+const Carrousel = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 500
+    //slidesToShow: 1,
+    //slidesToScroll: 1
+  };
+
+  return (
+    <div
+      className="carrousel_wrapper"
+      style={{ height: `${window.innerHeight}px`, overflow: 'hidden' }}
+    >
+      <Slider {...settings}>
+        <div>
+          <div
+            style={{
+              background: `url(${slide_one})`,
+              height: `${window.innerHeight}px`
+            }}
+            className="carrousel_image"
+          />
+        </div>
+        <div>
+          <div
+            style={{
+              background: `url(${slide_two})`,
+              height: `${window.innerHeight}px`
+            }}
+            className="carrousel_image"
+          />
+        </div>
+        <div>
+          <div
+            style={{
+              background: `url(${slide_three})`,
+              height: `${window.innerHeight}px`
+            }}
+            className="carrousel_image"
+          />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Carrousel;
