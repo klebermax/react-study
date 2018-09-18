0996d7581259a70aaa8a2eeb2a47287f54788a0a
Kleber Maximiano <the.key.max@gmail.com>
2018-09-18 16:35:11 -0300

    Admin Matches List

--- m_city/src/routes.js
+++ m_city/src/routes.js
@@ -2,18 +2,23 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 
+import PrivateRoute from './Components/authRoutes/privateRoutes';
+import PublicRoute from './Components/authRoutes/publicRoutes';
+
 import Home from './Components/home';
 import SignIn from './Components/signin';
 
 import Dashboard from './Components/admin/Dashboard';
+import AdminMatches from './Components/admin/matches';
 
 const Routes = props => {
   return (
     <Layout>
       <Switch>
-        <Route exact component={Dashboard} path="/dashboard" />
-        <Route exact component={SignIn} path="/sign_in" />
-        <Route exact component={Home} path="/" />
+        <PrivateRoute {...props} path="/dashboard" exact component={Dashboard} />
+        <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches} />
+        <PublicRoute {...props} restricted={true} exact component={SignIn} path="/sign_in" />
+        <PublicRoute {...props} restricted={false} exact component={Home} path="/" />
       </Switch>
     </Layout>
   );
